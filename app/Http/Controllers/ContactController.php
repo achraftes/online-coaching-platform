04f1cd17c06f7d7d1bc72e04aa263bfc52a10b99
achraf <achraf04chke@gmail.com>
2025-04-18 23:29:27 +0100

    Fix: Scroll to contact section after form submission and show success alert

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -23,6 +23,7 @@ class ContactController extends Controller
 
         Mail::to('achraf04chke@gmail.com')->send(new \App\Mail\ContactMessage($details));
 
-        return back()->with('success', 'Your message has been sent successfully!');
+        return redirect()->to(url()->previous() . '#contacts')->with('success', 'Your message has been sent successfully!');
+
     }
 }
