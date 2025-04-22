49b76c13351af04dcd87fc4af499746cc55a8b10
achraf <achraf04chke@gmail.com>
2025-04-22 01:23:50 +0100

    feat(auth): add POST route for user logout

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,6 @@ Route::post('/send-message', [ContactController::class, 'send'])->name('contact.
 
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
