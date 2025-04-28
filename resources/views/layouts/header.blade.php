4ff7f8ea49e293867c1adaf791b1572cab2d3a99
achraf <achraf04chke@gmail.com>
2025-04-28 02:39:00 +0100

    delete title  for page  heder

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Header avec Profil</title>
+    <title>Header </title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
