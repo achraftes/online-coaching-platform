d2f284f669cc2f88d6ada9e6c2ecaed874e017de
achraf <achraf04chke@gmail.com>
2025-04-30 01:28:11 +0100

    add  url test to  footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -18,8 +18,8 @@
           <li><a href="/" class="text-gray-400 hover:text-white">Home</a></li>
           <li><a href="#blogs" class="text-gray-400 hover:text-white">Blogs</a></li>
           <li><a href="#services" class="text-gray-400 hover:text-white">Services</a></li>
-          <li><a href="#" class="text-gray-400 hover:text-white">Contact</a></li>
-          <li><a href="#contacts" class="text-gray-400 hover:text-white">Test</a></li>
+          <li><a href="#contacts" class="text-gray-400 hover:text-white">Contact</a></li>
+          <li><a href="#features" class="text-gray-400 hover:text-white">Test</a></li>
         </ul>
       </div>
 
