fe136d1b1f8378cc063f948a5eb2ef62d0676e6d
achraf <achraf04chke@gmail.com>
2025-05-05 02:26:47 +0100

    update styel  for  page  login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -343,15 +343,10 @@
                     @enderror
                 </div>
                 
-                <div class="form-group">
+                <div class="form-group password-field">
                     <label for="password">Mot de passe</label>
-                    <div class="password-field">
-                        <input type="password" id="password" name="password" placeholder="Votre mot de passe" required>
-                        <svg class="eye-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 5C7.453 5 3.57 7.909 2 12C3.57 16.091 7.453 19 12 19C16.547 19 20.43 16.091 22 12C20.43 7.909 16.547 5 12 5Z" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </div>
+                    <input type="password" id="password" name="password" placeholder="••••••••" required>
+                    <span class="eye-icon" onclick="togglePasswordVisibility()">👁️</span>
                     @error('password')
                         <small style="color: #ff6b6b;">{{ $message }}</small>
                     @enderror
@@ -366,53 +361,33 @@
                 </div>
                 
                 <button type="submit" class="login-btn">Se connecter</button>
-            </form>
-            
-            <div class="or-divider">Ou</div>
-            
-            <div class="social-signup">
-                <a href="#" class="social-btn">
-                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
-                        <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
-                        <path d="M5.84 14.1c-.22-.66-.35-1.36-.35-2.1s.13-1.44.35-2.1V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.61z" fill="#FBBC05"/>
-                        <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
-                    </svg>
-                    Continuer avec Google
-                </a>
                 
-                <a href="#" class="social-btn">
-                    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="#1877F2"/>
-                        <path d="M16.671 15.073l.532-3.47h-3.328v-2.25c0-.949.465-1.874 1.956-1.874h1.513V4.526s-1.374-.235-2.686-.235c-2.741 0-4.533 1.662-4.533 4.669v2.672H7.078v3.47h3.047v8.385c.61.093 1.234.14 1.87.14s1.26-.047 1.87-.14v-8.385h2.796z" fill="white"/>
-                    </svg>
-                    Continuer avec Facebook
-                </a>
-            </div>
+                <div class="or-divider">OU</div>
+                
+                <div class="social-signup">
+                    <a href="{{ route('login.google') }}" class="social-btn">
+                        <img src="https://img.icons8.com/ios/50/000000/google-logo.png" alt="Google Logo"> Connexion avec Google
+                    </a>
+                    <a href="{{ route('login.facebook') }}" class="social-btn">
+                        <img src="https://img.icons8.com/ios/50/000000/facebook-new.png" alt="Facebook Logo"> Connexion avec Facebook
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
 
     <script>
-        // Script pour basculer la visibilité du mot de passe
-        document.querySelector('.eye-icon').addEventListener('click', function() {
+        function togglePasswordVisibility() {
             const passwordInput = document.getElementById('password');
-            const currentType = passwordInput.type;
-            
-            if (currentType === 'password') {
-                passwordInput.type = 'text';
-                this.innerHTML = `
-                    <path d="M2 12C2 12 5 5 12 5C19 5 22 12 22 12C22 12 19 19 12 19C5 19 2 12 2 12Z" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M3 3L21 21" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                `;
+            const icon = document.querySelector('.eye-icon');
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                icon.textContent = "🙈";
             } else {
-                passwordInput.type = 'password';
-                this.innerHTML = `
-                    <path d="M12 5C7.453 5 3.57 7.909 2 12C3.57 16.091 7.453 19 12 19C16.547 19 20.43 16.091 22 12C20.43 7.909 16.547 5 12 5Z" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#9a9aa8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                `;
+                passwordInput.type = "password";
+                icon.textContent = "👁️";
             }
-        });
+        }
     </script>
 </body>
 </html>
