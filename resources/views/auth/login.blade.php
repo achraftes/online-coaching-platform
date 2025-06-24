03d36d157c4cf2153770be113b3446f94d5b6146
achraf <achraf04chke@gmail.com>
2025-06-25 00:52:23 +0100

    chnge  afunction  for a csrf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
     <link rel="apple-touch-icon" href="{{ asset('images/only coach (1).png') }}">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token(77) }}">
     
     <style>
         * {
