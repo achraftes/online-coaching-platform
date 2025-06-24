d0d25fad478cd07f5c77648f47a233d376899797
achraf <achraf04chke@gmail.com>
2025-06-25 00:56:02 +0100

    chnage  styale for container

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,7 +33,7 @@
         .container {
             display: flex;
             width: 100%;
-            max-width: 900px;
+            max-width: 90088px;
             height: 600px;
             border-radius: 12px;
             overflow: hidden;
