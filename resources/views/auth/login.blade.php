5a98cbbf917824eb7bf114614280f7da1a3f18e2
achraf <achraf04chke@gmail.com>
2025-06-30 23:46:04 +0100

    chnage stayle  for page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,9 +15,9 @@
     
     <style>
         * {
-            box-sizing: border-box;
+            /* box-sizing: border-box;
             margin: 0;
-            padding: 0;
+            padding: 0; */
         }
         
         body {
