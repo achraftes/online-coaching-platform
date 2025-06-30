8501293254b27465b4453fe837b5231e1a177f65
achraf <achraf04chke@gmail.com>
2025-06-30 23:49:18 +0100

    update  stayle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
             left: 0;
             right: 0;
             background: linear-gradient(to top, rgba(0,0,0,0.8), transparent);
-            padding: 40px;
+            padding: 41px;
             color: white;
             text-align: center;
         }
