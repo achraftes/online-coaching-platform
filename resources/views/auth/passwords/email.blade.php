e600d4f0279a2b440dc08f1af832a4ec78c1bc6c
achraf <achraf04chke@gmail.com>
2025-07-01 01:11:50 +0100

    update style  for container

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -29,7 +29,7 @@
             min-height: 100vh;
             display: flex;
             justify-content: center;
-            /* align-items: center; */
+            align-items: center;
         }
 
         /* Logo area */
