aa1636ce3009160cba2fcc2a9c995679a9be5d17
achraf <achraf04chke@gmail.com>
2025-07-02 01:03:48 +0100

    chnage stayle for page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -159,7 +159,7 @@
             background-color: #c6f6d5;
             color: #276749;
             border: none;
-            border-radius: 9px;
+            border-radius: 8px;
             padding: 1rem;
             margin-bottom: 1.5rem;
             display: flex;
