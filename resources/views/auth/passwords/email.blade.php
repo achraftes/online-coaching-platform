42bf7f4c0fc1b066a284f74764ede7cd96bce94a
achraf <achraf04chke@gmail.com>
2025-07-06 01:38:53 +0100

    make  icon for name  titele for page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reset Your </title>
+    <title>Reset Your Password</title>
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/only coach (1).png') }}">
     <link rel="shortcut icon" href="{{ asset('images/only coach (1).png') }}">
     <link rel="apple-touch-icon" href="{{ asset('images/only coach (1).png') }}">
