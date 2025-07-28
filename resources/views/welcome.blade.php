fca6347c6230a23a8c927dfd354894355ba33fe1
achraf <achraf04chke@gmail.com>
2025-07-29 00:13:52 +0100

    delete link for  script fille

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.2/tailwind.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     
-    <script src="https://js.stripe.com/v3/"></script>
     <script>
         // Fonction pour charger PayPal de manière dynamique
         function loadPayPalScript() {
