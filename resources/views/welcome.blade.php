dc8c4dea2aa6943fbdb4df5b5fd676160738d190
achraf <achraf04chke@gmail.com>
2025-07-28 17:18:23 +0100

    update  message  for notification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
                 script.async = true;
 
                 script.onload = () => {
-                    console.log('Script PayPal chargé avec succè');
+                    console.log('Script PayPal chargé avec succès');
                     // Attendre un court instant pour s'assurer que PayPal est bien initialisé
                     setTimeout(() => {
                         if (window.paypal) {
