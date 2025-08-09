55c63765ba0776f4809a1b7bc48c9775e51689f9
achraf <achraf04chke@gmail.com>
2025-08-09 19:18:13 +0100

    update  border-radius

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -29,7 +29,7 @@
             margin: 50px auto;
             padding: 30px;
             background: #fff;
-            border-radius: 10px;
+            border-radius: 100px;
             box-shadow: 0 0 20px rgba(0,0,0,0.1);
         }
         
