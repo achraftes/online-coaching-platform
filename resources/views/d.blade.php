32ec1eac83d4af711f65590f621ad2425a12a27e
achraf <achraf04chke@gmail.com>
2025-08-12 00:11:49 +0100

    delete a comment

--- resources/views/d.blade.php
+++ resources/views/d.blade.php
@@ -10,7 +10,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <style>
         /* Cacher les SVG sur petits écrans */
-        /* Cacher les SVG sur petits écrans */
         @media(max-width:1520px) {
             .left-svg {
                 display: none;
