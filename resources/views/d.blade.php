6a4ba5564e63475e8c459ac41c4e24a445c36e53
achraf <achraf04chke@gmail.com>
2025-08-11 02:01:14 +0100

    add  new  name  for  title

--- resources/views/d.blade.php
+++ resources/views/d.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Coaching Profl</title>
+    <title>Coaching Professionel</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.2/tailwind.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <style>
