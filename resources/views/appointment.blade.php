7624a08d93f5c28890516a17dc1e69fa8fc0a358
achraf <achraf04chke@gmail.com>
2025-08-19 01:11:54 +0100

    update  stayle  for this  page

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -64,12 +64,12 @@
             margin-bottom: 20px;
         }
 
-        .form-group label {
+        /* .form-group label {
             display: block;
             font-weight: 600;
             margin-bottom: 8px;
             color: #4a5568;
-        }
+        } */
 
         .form-group input[type="datetime-local"],
         .form-group textarea {
